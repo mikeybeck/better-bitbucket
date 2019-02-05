ee6ffe8ee202e0159484d916150a276877cbea21
Mike Beck <mike@mikeybeck.com>
2019-02-05 23:17:37 +1300

    Highlight filename in sidebar of currently viewed file(s)

--- inject.js
+++ inject.js
@@ -25,4 +25,67 @@
         clone.id = 'commit-files-summary-sidebar';
         document.body.appendChild(clone);
     }
+
+
+
+
+
+    // TESTING BELOW HERE:
+
+    function isElementInViewport(el) {
+        var rect = el.getBoundingClientRect();
+
+        return (
+            rect.top >= 0 &&
+            rect.left >= 0 &&
+            rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) && /*or $(window).height() */
+            rect.right <= (window.innerWidth || document.documentElement.clientWidth) /*or $(window).width() */
+        );
+    }
+
+    function onVisibilityChange(el, callback) {
+        var old_visible;
+        return function () {
+            var visible = isElementInViewport(el);
+            if (visible != old_visible) {
+                old_visible = visible;
+                if (typeof callback == 'function') {
+                    callback(visible);
+                }
+            }
+        }
+    }
+
+
+    let els = document.getElementsByClassName('diff-actions');
+    els = Array.prototype.slice.call(els);
+
+    els.forEach(el => {
+        var handler = onVisibilityChange(el, function (visible) {
+            console.log('visiblilty changed!');
+            console.log(visible);
+            console.log(el.id);
+            console.log(els.indexOf(el));
+
+            let c = document.getElementById('commit-files-summary-sidebar').children;
+            if (visible) {
+                c[els.indexOf(el)].style.backgroundColor = 'yellow';
+            } else {
+                c[els.indexOf(el)].style.backgroundColor = 'inherit';
+            }
+        });
+
+        if (window.addEventListener) {
+            addEventListener('DOMContentLoaded', handler, false);
+            addEventListener('load', handler, false);
+            addEventListener('scroll', handler, false);
+            addEventListener('resize', handler, false);
+        } else if (window.attachEvent) {
+            attachEvent('onDOMContentLoaded', handler); // IE9+ :(
+            attachEvent('onload', handler);
+            attachEvent('onscroll', handler);
+            attachEvent('onresize', handler);
+        }
+    });
+
 })();
